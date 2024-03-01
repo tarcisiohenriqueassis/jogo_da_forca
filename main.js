3be82ee3b492c0074619bced57431c651da1c50a
teste <tarcisiohenriqueassis@gmail.com>
2024-03-01 16:30:58 -0300

    adicionado nome usuario

--- main.js
+++ main.js
@@ -1,3 +1,13 @@
 import entradaDados from 'readline-sync';
 
 
+let trasejado = " ";
+trasejado = trasejado.padEnd(30,"-");
+
+console.log(trasejado+" JOGO DA FORCA "+trasejado);
+
+let nomeUsuario=entradaDados.question("Digite seu nome: ")
+console.log(`\nBem vindo ${nomeUsuario}\n`);
+
+
+
